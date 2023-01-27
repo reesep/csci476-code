3f235e822bc0093717fa6671b169e61ec88b4298
Reese Pearsall <reesepearsallcs@gmail.com>
2023-01-27 09:19:36 -0700

    Create hello.c

---
+++ 00_intro/hello.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void) {
+    
+  print("Hello from another C progam!\n");
+  for(int i = 0; i < 30; i++){
+    
+    printf("%d\n",i);
+  }
+  
+}
